935e14d99bd416729d2f0ad829da7f6a89b0c9aa
Micah Njeru <micahnjeru1@gmail.com>
2023-06-03 19:27:36 +0300

    Initial commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Micah Jesse Njeru on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
